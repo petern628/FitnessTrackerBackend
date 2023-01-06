701b4d7ea48bc09141c5939baa4549f1250fccfb
thekidsparrow <aveshart@gmail.com>
2023-01-06 14:53:37 -0600

    Fixing server issues

--- api/activities.js
+++ api/activities.js
@@ -1,9 +1,30 @@
+
 const express = require('express');
 const router = express.Router();
+
+
+
+router.use((req, res, next ) =>{
+    console.log("A request is being made to /activities")
+
+    next();
+});
 
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
+
+// router.get('/', async (req, res) => {
+//    // const allPosts = await getAllPosts();
+
+//     const posts = allPosts.filter(post => {
+//         return post.active || (req.user && post.author.id === req.user.id);
+//       });
+
+//     res.send({
+//       "posts": posts
+//     });
+//   });
 
 // POST /api/activities
 
--- api/index.js
+++ api/index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const PORT = 3000;
-const express = require('express');
-const server = express();
-
-server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
-});
-
 // GET /api/health
-router.get('/health', async (req, res, next) => {
-});
+////router.get('/health', async (req, res, next) => {
+//});
 
 // ROUTER: /api/users
 const usersRouter = require('./users');
