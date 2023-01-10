348c7bcd10b839a419f487a80d2413a081c913ab
PeterN <peternguyen628@yahoo.com>
2023-01-10 15:59:19 -0600

    add error handler

--- app.js
+++ app.js
@@ -13,4 +13,30 @@ app.use(cors())
 const router = require('./api');
 app.use('/api', router);
 
+app.get((req, res, next) => {
+    res.status(404);
+
+    next({
+        name: '404 - Not Found',
+        message: 'No route found for the requested URL',
+    });
+});
+
+app.use((error, req, res, next) => {
+    console.error('SERVER ERROR: ', error);
+    if (res.statusCode < 400) res.status(500);
+    console.log({
+        error: error.message,
+        name: error.name,
+        message: error.message,
+        table: error.table,
+    });
+    res.send({
+        error: error.message,
+        name: error.name,
+        message: error.message,
+        table: error.table,
+    });
+});
+
 module.exports = app;
